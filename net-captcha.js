543552834c4f29d10ce381c59adb907de6fdfa86
netanel <phpnetanel@gmail.com>
2014-08-08 14:18:29 +0300

    netCaptcha

---
+++ net-captcha.js
@@ -0,0 +1,24 @@
+angular.module('netCaptcha', [])
+
+.directive('netCaptcha', function(){
+
+  return{
+    require:'ngModel',
+    link: function(scope, elem, attrs, ctrl){
+      ctrl.$parsers.unshift(checkCaptcha);
+             scope.numCapOne = Math.floor(Math.random() * 10) + 1;
+             scope.numCapTwo = Math.floor(Math.random() * 10) + 1;
+
+
+      function checkCaptcha(viewValue){
+        if (Number(elem.val()) == (scope.numCapOne + scope.numCapTwo)) {
+          ctrl.$setValidity('captcha',true);
+        }
+        else{
+          ctrl.$setValidity('captcha', false);
+        }
+        return viewValue;
+      }
+    }
+  };
+});
